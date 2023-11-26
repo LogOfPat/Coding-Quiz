0565ab7def7c927d1a7d970065609bd58a3edbd6
LogPat <1bagels2@gmail.com>
2023-11-26 17:35:42 -0600

    Basic functions created

---
+++ script.js
@@ -0,0 +1,237 @@
+/*
+psuedo-code
+
+create
+questions database: questions [answers = {"answer1":true, answer2:false...}]
+score
+
+Load website
+Display:
+button 'view highscore'
+'timer'
+button 'start quiz'
+
+click 'start quiz'
+    initialize
+    score = 0
+    timer = 5
+    answeredQuestions = 0;
+    if (answeredQuestions < questions.length) or timer = 0 { 
+        load question and answer
+            click answer
+            if right
+                score++
+            else
+                timer--
+    }
+    else
+        set score
+        replay again
+
+*/ 
+
+// The questions and answers to be used
+const questionSet = [
+    question1 = "q1",
+    question2 = "q2",
+    question3 = "q3",
+    question4 = "q4",
+    question5 = "q5",
+    question6 = "q6"
+];
+
+const answerSet =[
+    answers1 = [
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholderTrue",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ],
+    answers2 = [ 
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ],
+    answers3 = [
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ],
+    answers4 = [
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ],
+    answers5 = [
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ],
+    answers6 = [
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": true,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        },
+        {
+            "answer": "placeholder",
+            "truth": false,
+        }
+    ]
+]
+
+const quizTotal = [questionSet, answerSet];
+// object that'll be stored after the game ends
+let score = {
+    initials : "",
+    score : 0,
+};
+
+// Initializes vairables for the menus.  Sets start to show and the quiz and highscore menus to be hidden by default.
+
+var startMenu = document.getElementById("startMenu");
+var quizMenu = document.getElementById("quiz");
+var highscoreMenu = document.getElementById("highscore");
+var startButton = document.getElementById("startButton");
+
+// hides the quiz and highscore menus at the start.
+quizMenu.style.display = "none";
+highscoreMenu.style.display = "none";
+
+startButton.addEventListener("click", startGame);
+
+// document.getElementById("question").innerText = "It works!";  Can use this to replace content HTML elements.
+
+
+function startGame(event) {
+    event.stopPropagation();
+    startMenu.style.display = "none";
+    quizMenu.style.display = "block";
+    var timer = 6;
+    var set = 0;
+    runQuiz(timer, set);
+    
+};
+
+function runQuiz(timerCount, set) {
+
+    var q = document.getElementById("question");
+    var a1 = document.getElementById("answer1");
+    var a2 = document.getElementById("answer2");
+    var a3 = document.getElementById("answer3");
+    var a4 = document.getElementById("answer4");
+    var feedback = document.getElementById("feedback");
+    
+    // creates event listeners for the HTML elements
+    a1.addEventListener("click", function (event) {
+        checkAnswer(event, answer1, timerCount)
+    });
+    a2.addEventListener("click", function (event) {
+        checkAnswer(event, answer2, timerCount)
+    });
+    a3.addEventListener("click", function (event) {
+        checkAnswer(event, answer3, timerCount)
+    });
+    a4.addEventListener("click", function (event) {
+        checkAnswer(event, answer4, timerCount)
+    });
+
+    // selects the answers and questions from quizTotal
+    var question = quizTotal[0][set];
+    var answer1 = quizTotal[1][set][0];
+    var answer2 = quizTotal[1][set][1];
+    var answer3 = quizTotal[1][set][2];
+    var answer4 = quizTotal[1][set][3];
+    
+    
+    // changes the text of the buttons
+    q.innerText = question;
+    a1.innerText = answer1.answer;
+    a2.innerText = answer2.answer;
+    a3.innerText = answer3.answer;
+    a4.innerText = answer4.answer;
+};
+
+function checkAnswer(event, answer, timerCount) {
+    event.stopPropagation();
+    var feedback = document.getElementById("feedback");
+    var timer = timerCount;
+    console.log(answer);
+    if (answer.truth){
+        feedback.innerText = "Correct!"
+    }
+    else {
+        feedback.innerText = "Try again!"
+    }
+};
